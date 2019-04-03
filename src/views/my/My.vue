bc7ea7e13ff1c75a050a79b64a7fad6fd1e3401c
金志强 <641283231@qq.com>
2019-04-03 11:06:23 +0800

    my end

--- src/views/my/My.vue
+++ src/views/my/My.vue
@@ -5,17 +5,19 @@
 <template>
   <div >
     <UserCom class="user-com"></UserCom>
+    <UserTab></UserTab>
   </div>
 </template>
 
 <script>
 import UserCom from './components/UserCom'
-
+import UserTab from './components/UserTab'
 export default {
   name: 'my',
 
   components:{
     UserCom,
+    UserTab
   },
 
   data () {
---
+++ src/views/my/components/UserTab.vue
@@ -0,0 +1,42 @@
+<style lang='less' scoped>
+@import './UserTab.less';
+</style>
+
+<template>
+  <div class="user-tab">
+    <img v-for="item in images" :key="item" :src="item" alt="">
+  </div>
+</template>
+
+<script>
+import login01 from '@/assets/login/login01.png'
+import login02 from '@/assets/login/login02.png'
+import login03 from '@/assets/login/login03.png'
+import login04 from '@/assets/login/login04.png'
+import login05 from '@/assets/login/login05.png'
+export default {
+  name: 'usertab',
+
+  components:{},
+
+  data () {
+    return {
+      images: [
+        login01,
+        login02,
+        login03,
+        login04,
+        login05
+      ]
+    }
+  },
+
+  methods: {},
+
+  computed: {},
+
+  mounted(){
+  }
+}
+</script>
+
