4fc566e6aa7cba64b2f3e6d6ce49f41965e68490
金志强 <641283231@qq.com>
2019-04-04 17:45:10 +0800

    add end

--- src/views/add/Add.vue
+++ src/views/add/Add.vue
@@ -8,18 +8,47 @@
       v-model="show" 
       position="bottom" 
       :overlay="true"
-
     >
-      <Nav @onPublish="handlePublish"></Nav>
+      <Nav 
+        @onPublish="handlePublish"
+      >
+      </Nav>
+
+      <van-field 
+        v-model="comment" 
+        placeholder="说说你此刻的感受吧..."
+        class="comment" 
+      />
+
+
+      <div class="upload">
+        <van-row guttar="10">
+          <van-col span="6" v-for="item in images" :key="item" >
+            <img :src="item" @click="onCheakPhoto(item)" alt="">
+          </van-col>
+          <van-col span="6">
+            <van-uploader :after-read="onRead" class="upload-button">
+              <van-icon name="photograph" size="25px"/>
+            </van-uploader>
+          </van-col>
+        </van-row>
+      </div>
+      
+
     </van-popup>
   </div>
 </template>
 
 <script>
+import hashiqi1 from '@/assets/test/hashiqi1.jpg'
+import hashiqi2 from '@/assets/test/hashiqi2.jpg'
+import hashiqi3 from '@/assets/test/hashiqi3.jpg'
+import hashiqi4 from '@/assets/test/hashiqi4.jpg'
+import hashiqi5 from '@/assets/test/hashiqi5.jpg'
 import Nav from './components/Nav'
-import { Popup, } from 'vant';
+import { Popup, Field, Uploader, Row, Col, ImagePreview } from 'vant';
 import Vue from 'vue';
-Vue.use(Popup);
+Vue.use(Popup).use(Field).use(Uploader).use(Row).use(Col);
 export default {
   name: 'add',
 
@@ -27,16 +56,40 @@ export default {
     Nav
   },
 
+  props:{
+    images: {
+      type: Array,
+      default: () => [
+        hashiqi1,
+        hashiqi2,
+        hashiqi3,
+        hashiqi4,
+        hashiqi5,
+      ]
+    }
+  },
+
   data () {
     return {
-      show: false
+      show: false,
+      comment: ""
     }
   },
 
   methods: {
     handlePublish(){
-      
-    }
+
+    },
+    onRead(){
+
+    },
+    onCheakPhoto(item){
+      let current = this.images.indexOf(item)
+      ImagePreview({
+        images: this.images,
+        startPosition: current,
+      });
+    },
   },
 
   computed: {},
